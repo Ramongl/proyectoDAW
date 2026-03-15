55e81b254ae793f0ace4e6f40d2d1b60e7d1b3a4
Ramongl <ramongarcia1988@gmail.com>
2026-03-15 17:08:01 +0100

    Create MiBotonVenta.vue

---
+++ app/components/MiBotonVenta.vue
@@ -0,0 +1,15 @@
+<script setup>
+// Solo un bloque de script y una definición de props
+defineProps({
+  precio: {
+    type: String,
+    default: '0€'
+  }
+})
+</script>
+
+<template>
+  <button class="bg-pink-200 text-black px-6 py-2 rounded-md shadow-md hover:bg-gray-800 m-2">
+    Comprar por {{ precio }}
+  </button>
+</template>
