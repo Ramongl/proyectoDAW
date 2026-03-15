e0f13916c2715c799be2705453d435a1a00012ad
Ramongl <ramongarcia1988@gmail.com>
2026-03-15 17:25:40 +0100

    Update Destacados.vue

--- app/components/Destacados.vue
+++ app/components/Destacados.vue
@@ -1,33 +1,39 @@
 <template>
   <div class="p-6 md:p-10 bg-gray-50">
-    <h2 class="text-3xl mb-10 text-center text-gray-800 font-bold">Productos Destacados</h2>
+    <h2 class="text-3xl mb-10 text-center text-gray-800 font-bold">
+      Productos Destacados
+    </h2>
     
-    <div class="grid grid-cols-2 lg:grid-cols-4 gap-6 max-w-7xl mx-auto">
-      
-      <div v-for="n in 4" :key="n" class="bg-white border border-gray-100 flex flex-col items-center p-4 shadow-sm hover:shadow-md transition-shadow group">
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-8">
+      <div
+        v-for="n in 4"
+        :key="n"
+        class="bg-white border border-gray-200 flex flex-col items-center p-4 shadow-sm"
+      >
         
-        <div class="relative w-full aspect-[4/5] overflow-hidden mb-4 bg-gray-100 flex items-center justify-center text-gray-400">
-          
-          <img 
-            
-            alt="Producto de Cosmética"
-            class="absolute inset-0 w-full h-full object-cover object-center group-hover:scale-105"
-          />
-          
-          
-        </div>
-        
-        <h3 class="text-sm font-semibold uppercase text-gray-900 text-center mb-1">Nombre del Producto</h3>
-        <p class="text-xs text-gray-500 mb-3 text-center">Pequeña descripción del artículo</p>
-        
-        <div class="w-full flex items-center justify-between mt-auto pt-3 border-t border-gray-100">
-          <span class="text-lg font-bold text-gray-900">120€</span>
-          <button class="bg-gray-300 text-black font-bold text-[10px] px-4 py-2 hover:bg-rose-300 transition-colors">
-            Añadir
-          </button>
+          <div class="relative aspect-[3/4] overflow-hidden bg-gray-100 mb-4">
+            <img
+              src=""
+              alt="Producto de cosmética"
+              class="w-full h-full object-contain p-15"
+            />
+            <div class="absolute bottom-0 left-0 right-0 p-4">
+              <button
+                class="w-full py-2 bg-rose-300 text-white text-[10px] uppercase tracking-widest"
+              >
+                Comprar
+              </button>
+            </div>
+          </div>
+          <h3
+            class="text-xs font-bold uppercase tracking-tighter hover:text-pink-500"
+          >
+            Destacado n.{{ n }}
+          </h3>
+          <p class="text-gray-500 text-[15px] mb-2">Cuidado Facial Intensivo</p>
+          <span class="text-s font-semibold">45,00 €</span>
         </div>
       </div>
-      
-    </div>
+    
   </div>
 </template>
