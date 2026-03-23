f53a0fc601014cd43428c9ea9371892319b23dbd
Ramongl <ramongarcia1988@gmail.com>
2026-03-23 23:23:52 +0100

    Update BotonCompra.vue
    Revisión de estilos

--- app/components/BotonCompra.vue
+++ app/components/BotonCompra.vue
@@ -1,5 +1,5 @@
 <template>
   <button
-    class="w-full bg-black text-white py-2 uppercase text-xs tracking-widest hover:bg-rose-300"
+    class="w-full bg-black text-white py-2 uppercase text-xs tracking-widest hover:bg-rose-500"
   >Comprar</button>
 </template>
