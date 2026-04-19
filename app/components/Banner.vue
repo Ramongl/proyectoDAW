a4c257f11a97bf62c8c76aafcb79b4577e2237c5
Ramongl <ramongarcia1988@gmail.com>
2026-04-19 23:31:57 +0200

    Update Banner.vue
    se elimina la ruta a promociones, no se va a realizar dicho desarrollo

--- app/components/Banner.vue
+++ app/components/Banner.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="aspect-[16/5] hidden md:flex">
     
-    <NuxtLink to="/promociones">
+    
       <img
         class="w-full h-full object-cover object-center"
         src="~\assets\imagenes\banner.png"
       />
-    </NuxtLink>
+    
 
     
     
   </div>
     <div class="md:hidden">
     
-    <NuxtLink to="/promociones">
+    
       <img
         class="w-full h-full object-cover object-center"
         src="~\assets\imagenes\banner_movil.png"
       />
-    </NuxtLink>
+    
 
     
     
