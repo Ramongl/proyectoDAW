19f4cbe448e248c0978ebff367fb35e9a68f562d
Ramongl <ramongarcia1988@gmail.com>
2026-04-19 23:18:11 +0200

    Create buscar-todos-productos.get.ts
    query para sacar todos los productos

---
+++ server/api/buscar-todos-productos.get.ts
@@ -0,0 +1,17 @@
+import { db } from '../utils/db'
+
+export default defineEventHandler(async (event) => {
+  try {
+    const [rows] = await db.query('SELECT * FROM productos ORDER BY id DESC');
+
+    return rows;
+
+  } catch (error) {
+    console.error("Error en DB:", error);
+    
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Error interno al obtener los productos',
+    });
+  }
+});
