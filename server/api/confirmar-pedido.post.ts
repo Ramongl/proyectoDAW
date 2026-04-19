3bb6c5014ac68ab7468edec95dead6ec84309c17
Ramongl <ramongarcia1988@gmail.com>
2026-04-19 23:33:39 +0200

    Update confirmar-pedido.post.ts
    query para crear pedidos y lineas de pedido

--- server/api/confirmar-pedido.post.ts
+++ server/api/confirmar-pedido.post.ts
@@ -1,1 +1,26 @@
+import { db } from "../utils/db"; // 
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  const { usuario_id, productos, total } = body;
+
+  try {
+    const [resultPedido] = await db.query(
+      "INSERT INTO pedidos (usuario_id, total, estado) VALUES (?, ?, 'Pendiente')",
+      [usuario_id, total]
+    );
+    
+    const pedidoId = resultPedido.insertId;
+
+    for (const prod of productos) {
+      await db.query(
+        "INSERT INTO lineas_pedido (pedido_id, producto_id, cantidad, precio_unitario) VALUES (?, ?, ?, ?)",
+        [pedidoId, prod.id, prod.cantidad, prod.precio]
       );
+    }
+
+    return { message: "Pedido guardado con éxito", pedidoId };
+  } catch (error) {
+    throw createError({ statusCode: 500, message: "Error al guardar el pedido" });
+  }
+});
