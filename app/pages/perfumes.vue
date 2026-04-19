6d625bed9f48b8fc537f06c1c95ec3e76ef55e11
Ramongl <ramongarcia1988@gmail.com>
2026-04-19 23:29:21 +0200

    Update perfumes.vue
    Se incluyen variables para poder cambiar entre las diferentes subcategorias y poder conectarse a la bbdd para sacar dicha información

--- app/pages/perfumes.vue
+++ app/pages/perfumes.vue
@@ -4,8 +4,8 @@
     <header class="mb-12 text-center">
       <h1 class="text-4xl font-serif italic mb-4">Perfumes</h1>
       <p class="text-gray-500 max-w-2xl mx-auto text-sm tracking-wide">
-        Descubre esencias creadas por maestros perfumistas, 
-        desde extractos florales del sur de Francia hasta maderas preciosas de Oriente.
+        Descubre esencias creadas por maestros perfumistas, desde extractos
+        florales del sur de Francia hasta maderas preciosas de Oriente.
       </p>
     </header>
 
@@ -20,18 +20,27 @@
             </h3>
             <ul class="text-sm text-gray-600 space-y-3">
               <li
+                @click="cambiadorMujer"
+                :class="
+                  valorSub === 'mujer'
+                    ? 'text-rose-500 font-bold'
+                    : 'text-gray-600'
+                "
                 class="flex justify-between items-center hover:text-pink-500 cursor-pointer"
               >
                 Perfumes de mujer
-                <p class="text-[10px] text-gray-400"></p>
               </li>
               <li
                 class="flex justify-between items-center hover:text-pink-500 cursor-pointer"
+                @click="cambiadorHombre"
+                :class="
+                  valorSub === 'hombre'
+                    ? 'text-rose-500 font-bold'
+                    : 'text-gray-600'
+                "
               >
                 Perfumes de hombre
-                <p class="text-[10px] text-gray-400"></p>
               </li>
-
             </ul>
           </div>
         </div>
@@ -39,26 +48,72 @@
 
       <section class="flex-grow">
         <div class="grid grid-cols-2 md:grid-cols-3 gap-8">
-          <div v-for="i in 6" :key="i" class="group">
+          <div v-for="prod in productos" :key="prod.id" class="group">
             <div class="relative aspect-[3/4] overflow-hidden bg-gray-100 mb-4">
-              <img 
-                src="" 
-                  alt="Perfume"
-                  class="w-full h-full object-contain p-6"
+              <img
+                src=""
+                alt="Producto de perfumería"
+                class="w-full h-full object-contain p-6"
               />
               <div class="absolute bottom-0 left-0 right-0 p-4">
-                <BotonCompra></BotonCompra>
+                <BotonCompra :producto="prod"></BotonCompra>
               </div>
             </div>
-
-            
-            <h3 class="text-xs font-bold uppercase tracking-tighter">Eau de Parfum para Mujer v.{{i}}</h3>
-            <p class="text-gray-500 text-[15px] mb-2">Eau de Parfum para Mujer</p>
-            <span class="text-sm font-semibold">60,00 €</span>
+            <h3 class="text-xs font-bold uppercase tracking-tighter">
+              {{ prod.marca }}
+            </h3>
+            <p class="text-gray-500 text-[15px] mb-2">{{ prod.nombre }}</p>
+            <span class="text-sm font-semibold"
+              >{{ prod.precio.toString().replace(".", ",") }} €</span
+            >
           </div>
         </div>
-      </section>>
+      </section>
     </div>
   </div>
   <Footer></Footer>
 </template>
+
+<script setup>
+import { ref } from "vue";
+const valorCat = ref("perfumes");
+const valorSub = ref("0");
+const productos = ref([]);
+
+const cambiadorMujer = () => {
+  if (valorSub.value != "mujer") {
+    valorSub.value = "mujer";
+  } else if (valorSub.value == "mujer") {
+    valorSub.value = "0";
+  }
+  obtenerProductos();
+};
+
+const cambiadorHombre = () => {
+  if (valorSub.value != "hombre") {
+    valorSub.value = "hombre";
+  } else if (valorSub.value == "hombre") {
+    valorSub.value = "0";
+  }
+  obtenerProductos();
+};
+
+const obtenerProductos = async () => {
+  try {
+    const data = await $fetch("/api/buscar-prod-cateysub", {
+      method: "GET",
+      params: {
+        categoria: valorCat.value,
+        sub: valorSub.value,
+      },
+    });
+
+    productos.value = data;
+  } catch (error) {
+    console.error("Error al obtener productos:", error);
+    alert("No se pudieron cargar los productos");
+  } finally {
+  }
+};
+obtenerProductos();
+</script>
