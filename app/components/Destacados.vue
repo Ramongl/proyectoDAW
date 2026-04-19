44ccdbe32dbaca9e19a74553e92822292bd23032
Ramongl <ramongarcia1988@gmail.com>
2026-04-19 23:26:36 +0200

    Update Destacados.vue
    se añade al componente la lógica para acceder a la bbdd y coger los 4 productos más vendidos y con stock

--- app/components/Destacados.vue
+++ app/components/Destacados.vue
@@ -6,8 +6,8 @@
     
     <div class="grid grid-cols-2 md:grid-cols-4 gap-8">
       <div
-        v-for="n in 4"
-        :key="n"
+        v-for="prod in productos"
+        :key="prod.id"
         class="bg-white border border-gray-200 flex flex-col items-center p-4 shadow-sm"
       >
         
@@ -18,7 +18,7 @@
               class="w-full h-full object-contain p-15"
             />
             <div class="absolute bottom-0 left-0 right-0 p-4">
-              <BotonCompra></BotonCompra>
+              <BotonCompra :producto="prod"></BotonCompra>
                 
             
             </div>
@@ -26,12 +26,34 @@
           <h3
             class="text-xs font-bold uppercase tracking-tighter hover:text-pink-500"
           >
-            Destacado n.{{ n }}
+            {{ prod.marca }}
           </h3>
-          <p class="text-gray-500 text-[15px] mb-2">Cuidado Facial Intensivo</p>
-          <span class="text-s font-semibold">45,00 €</span>
+          <p class="text-gray-500 text-[15px] mb-2"> {{ prod.nombre }} </p>
+          <span class="text-s font-semibold"> {{ String(prod.precio).replace('.', ',') }} €</span>
         </div>
       </div>
     
   </div>
 </template>
+<script setup>
+import { ref } from "vue";
+const productos = ref([]);
+
+
+
+const obtenerProductos = async () => {
+  try {
+    const data = await $fetch("/api/productos-destacados", {
+      method: "GET",
+      
+    });
+
+    productos.value = data;
+  } catch (error) {
+    console.error("Error al obtener productos:", error);
+    alert("No se pudieron cargar los productos");
+  } finally {
+  }
+};
+obtenerProductos();
+</script>
