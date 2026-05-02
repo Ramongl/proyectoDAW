c60374350f401a750ef96aab004a91a38f51ad7f
Ramongl <ramongarcia1988@gmail.com>
2026-05-02 18:21:51 +0200

    Update cuenta.vue
    Se añade autentificacion frente a la base de datos y se genera la cookie si finalmente se loguea. Además se añade la funcionalidad de login sobre la plantilla inicial

--- app/pages/cuenta.vue
+++ app/pages/cuenta.vue
@@ -6,27 +6,30 @@
     <h1 class="text-2xl font-bold mb-6 uppercase">Mi Cuenta</h1>
 
     <div class="grid grid-cols-1 md:grid-cols-2 gap-10">
-      <div class="space-y-4">
-        <h2 class="font-bold uppercase text-xs tracking-widest text-gray-500">
-          Iniciar Sesión
-        </h2>
-        <input
-          type="email"
-          placeholder="Email"
-          class="w-full border p-2 focus:ring-1 focus:ring-pink-300 outline-none"
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          class="w-full border p-2 focus:ring-1 focus:ring-pink-300 outline-none"
-        />
-        <button
-          class="w-full bg-black text-white py-2 uppercase text-xs tracking-widest hover:bg-rose-300"
-        >
-          Entrar
-        </button>
-      </div>
-
+      <form @submit.prevent="login">
+        <div class="space-y-4">
+          <h2 class="font-bold uppercase text-xs tracking-widest text-gray-500">
+            Iniciar Sesión
+          </h2>
+          <input
+            v-model="formulario.email"
+            type="email"
+            placeholder="Email"
+            class="w-full border p-2 focus:ring-1 focus:ring-pink-300 outline-none"
+          />
+          <input
+            v-model="formulario.password"
+            type="password"
+            placeholder="Contraseña"
+            class="w-full border p-2 focus:ring-1 focus:ring-pink-300 outline-none"
+          />
+          <button
+            class="w-full bg-black text-white py-2 uppercase text-xs tracking-widest hover:bg-rose-300"
+          >
+            Entrar
+          </button>
+        </div>
+      </form>
       <div class="bg-gray-50 p-6 flex flex-col justify-center">
         <p class="text-sm text-gray-600 mb-4 italic">¿Aún no eres miembro?</p>
         <button
@@ -41,4 +44,49 @@
 </template>
 
 <script setup>
+
+const authCookie = useCookie("auth_user");
+
+
+const mirarCookie = () => {
+  
+  if (authCookie.value) {
+    const rol = authCookie.value.rol;
+    if (rol === "admon") {
+      navigateTo("/administracion");
+    } else if (rol === "user") {
+      navigateTo("/");
+    }
+  }
+};
+mirarCookie ();
+
+const formulario = ref({
+  email: "",
+  password: "",
+});
+
+
+
+const login = async () => {
+  try {
+    const usuarioLogueado = await $fetch("/api/loguear-usuario", {
+      method: "POST",
+      body: formulario.value,
+    });
+
+    const authCookie = useCookie("auth_user");
+    authCookie.value = usuarioLogueado;
+    if (authCookie.value.rol=="admon") {
+      navigateTo('/administracion')
+      }
+      else if(authCookie.value.rol=="user") 
+      {
+      navigateTo('/')
+      }
+  } catch (error) {
+    console.error("Error en el registro:", error);
+    alert(error.data?.message || "Error al registrarse");
+  }
+};
 </script>
