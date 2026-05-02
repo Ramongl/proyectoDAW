3d491bff81b80391feb6b734d4c18ab109671a71
Ramongl <ramongarcia1988@gmail.com>
2026-05-02 18:23:01 +0200

    Update carrito.vue
    modificación de la vista carrito y se añade la cookie

--- app/pages/carrito.vue
+++ app/pages/carrito.vue
@@ -5,9 +5,7 @@
       class="mb-8 p-6 bg-white border border-gray-200 rounded-lg flex items-center justify-between"
     >
       <div>
-        <h1 class="text-2xl font-bold uppercase text-black">
-          Tu Carrito
-        </h1>
+        <h1 class="text-2xl font-bold uppercase text-black">Tu Carrito</h1>
       </div>
       <NuxtLink
         to="/"
@@ -22,13 +20,12 @@
         <div
           class="px-6 py-3 bg-gray-900 rounded-t-lg border-b border-gray-800 text-white flex justify-between items-center"
         >
-          <h3 class="text-xs font-bold uppercase tracking-widest">
-            Artículos
-          </h3>
-
+          <h3 class="text-xs font-bold uppercase tracking-widest">Artículos</h3>
         </div>
 
         <div
+          v-for="item in carrito.productos"
+          :key="item.id"
           class="bg-white p-5 border border-gray-200 rounded-lg flex flex-col sm:flex-row items-center gap-4"
         >
           <div class="flex items-center gap-4 flex-1">
@@ -41,11 +38,9 @@
               <p
                 class="text-xs text-rose-500 uppercase font-bold tracking-tighter"
               >
-                Sérum Facial
+                {{ item.nombre }}
               </p>
-              <p class="text-sm font-bold text-black">
-                Sérum Facial 
-              </p>
+              <p class="text-sm font-bold text-black">{{ item.marca }}</p>
             </div>
           </div>
 
@@ -56,129 +51,26 @@
               class="flex items-center space-x-1 bg-gray-50 p-1.5 rounded-lg border border-gray-200"
             >
               <button
+                @click="carrito.reducir(item.id)"
                 class="w-7 h-7 flex items-center justify-center bg-white border border-gray-300 rounded text-black hover:bg-gray-100 font-bold text-sm"
               >
                 -
               </button>
-              <input
-                type="number"
-                value="1"
-                class="w-10 text-center border-none bg-transparent text-sm font-bold focus:ring-0"
-                readonly
-              />
+              <p>{{ item.cantidad }}</p>
               <button
+                @click="carrito.anadir(item.id)"
                 class="w-7 h-7 flex items-center justify-center bg-white border border-gray-300 rounded text-black hover:bg-gray-100 font-bold text-sm"
               >
                 +
               </button>
             </div>
             <p class="text-sm font-bold text-black w-20 text-right">
-              XX.XX €
+              {{ (item.precio * item.cantidad).toFixed(2).replace(".", ",") }} €
             </p>
-            <button class="text-black hover:text-rose-500 text-sm p-1">
-              X
-            </button>
-          </div>
-        </div>
-
-        <div
-          class="bg-white p-5 border border-gray-200 rounded-lg shadow-sm flex flex-col sm:flex-row items-center gap-4 hover:border-rose-100 transition-colors"
-        >
-          <div class="flex items-center gap-4 flex-1">
-            <div
-              class="h-16 w-16 bg-gray-100 rounded border border-gray-200 flex items-center justify-center text-black text-xs"
+            <button
+              @click="carrito.quitar(item.id)"
+              class="text-black hover:text-rose-500 text-sm p-1"
             >
-              IMG
-            </div>
-            <div class="flex-1">
-              <p
-                class="text-xs text-rose-500 uppercase font-bold tracking-tighter"
-              >
-                Crema Hidratante
-              </p>
-              <p class="text-sm font-bold text-black">
-                Crema Hidratante 50ml
-              </p>
-            </div>
-          </div>
-          <div
-            class="flex items-center gap-6 w-full sm:w-auto justify-between sm:justify-end"
-          >
-            <div
-              class="flex items-center space-x-1 bg-gray-50 p-1.5 rounded-lg border border-gray-200"
-            >
-              <button
-                class="w-7 h-7 flex items-center justify-center bg-white border border-gray-300 rounded text-black hover:bg-gray-100 font-bold text-sm"
-              >
-                -
-              </button>
-              <input
-                type="number"
-                value="1"
-                class="w-10 text-center border-none bg-transparent text-sm font-bold focus:ring-0"
-                readonly
-              />
-              <button
-                class="w-7 h-7 flex items-center justify-center bg-white border border-gray-300 rounded text-black hover:bg-gray-100 font-bold text-sm"
-              >
-                +
-              </button>
-            </div>
-            <p class="text-sm font-bold text-black w-20 text-right">
-              XX.XX €
-            </p>
-            <button class="text-black hover:text-rose-500 text-sm p-1">
-              X
-            </button>
-          </div>
-        </div>
-        <div
-          class="bg-white p-5 border border-gray-200 rounded-lg shadow-sm flex flex-col sm:flex-row items-center gap-4 hover:border-rose-100 transition-colors"
-        >
-          <div class="flex items-center gap-4 flex-1">
-            <div
-              class="h-16 w-16 bg-gray-100 rounded border border-gray-200 flex items-center justify-center text-black text-xs"
-            >
-              IMG
-            </div>
-            <div class="flex-1">
-              <p
-                class="text-xs text-rose-500 uppercase font-bold tracking-tighter"
-              >
-                Crema Hidratante
-              </p>
-              <p class="text-sm font-bold text-black">
-                Crema Hidratante 50ml
-              </p>
-            </div>
-          </div>
-          <div
-            class="flex items-center gap-6 w-full sm:w-auto justify-between sm:justify-end"
-          >
-            <div
-              class="flex items-center space-x-1 bg-gray-50 p-1.5 rounded-lg border border-gray-200"
-            >
-              <button
-                class="w-7 h-7 flex items-center justify-center bg-white border border-gray-300 rounded text-black hover:bg-gray-100 font-bold text-sm"
-              >
-                -
-              </button>
-              <input
-                type="number"
-                value="1"
-                class="w-10 text-center border-none bg-transparent text-sm font-bold focus:ring-0"
-                readonly
-              />
-              <button
-                class="w-7 h-7 flex items-center justify-center bg-white border border-gray-300 rounded text-black hover:bg-gray-100 font-bold text-sm"
-              >
-                +
-              </button>
-            </div>
-            <p class="text-sm font-bold text-black w-20 text-right">
-              XX.XX €
-            </p>
-            <button class="text-black hover:text-rose-500 text-sm p-1">
               X
             </button>
           </div>
@@ -190,9 +82,7 @@
           class="bg-white border border-gray-200 rounded-lg shadow-sm overflow-hidden sticky top-8"
         >
           <div class="px-4 py-3 bg-gray-50 border-b border-gray-200">
-            <h3
-              class="text-xs font-bold text-black uppercase tracking-wider"
-            >
+            <h3 class="text-xs font-bold text-black uppercase tracking-wider">
               Resumen de Compra
             </h3>
           </div>
@@ -200,14 +90,23 @@
           <div class="p-6 space-y-4">
             <div class="flex justify-between text-sm text-black">
               <span>Subtotal</span>
-              <span class="font-medium text-black">XX.XX €</span>
+              <span class="font-medium text-black"
+                >{{ (totalPrecio / 1.21).toFixed(2).replace(".", ",") }} €</span
+              >
             </div>
 
             <div
               class="flex justify-between text-sm text-black border-b border-gray-100 pb-4"
             >
               <span>Impuestos (IVA 21%)</span>
-              <span class="font-medium text-black">XX.XX €</span>
+              <span class="font-medium text-black"
+                >{{
+                  (totalPrecio.toFixed(2) - totalPrecio / 1.21)
+                    .toFixed(2)
+                    .replace(".", ",")
+                }}
+                €</span
+              >
             </div>
 
             <div class="flex justify-between pt-2">
@@ -217,16 +116,16 @@
                 Total del pedido
               </p>
               <p class="text-3xl font-extrabold text-black tracking-tight">
-                XX.XX €
+                {{ totalPrecio.toFixed(2).replace(".", ",") }} €
               </p>
             </div>
 
             <button
-              class="w-full bg-black  text-white px-6 py-2.5 text-[10px] font-bold uppercase hover:bg-rose-400 flex items-center justify-center gap-2"
+              class="w-full bg-black text-white px-6 py-2.5 text-[10px] font-bold uppercase hover:bg-rose-400 flex items-center justify-center gap-2"
+              @click="finalizarCompra"
             >
               Tramitar Pedido
             </button>
-
           </div>
         </div>
       </div>
@@ -234,3 +133,42 @@
   </div>
   <Footer></Footer>
 </template>
+
+<script setup>
+import { carrito, totalPrecio } from "~/utils/carrito";
+
+const authCookie = useCookie("auth_user");
+
+const vaciarCarrito = () => {
+  carrito.productos = [];
+};
+
+
+const finalizarCompra = async () => {
+  if (!authCookie.value) {
+    alert("Debes iniciar sesión para comprar");
+    return navigateTo("/cuenta");
+  }
+  
+
+  try {
+    const respuesta = await $fetch("/api/confirmar-pedido", {
+      method: "POST",
+      body: {
+        usuario_id: authCookie.value.id, 
+        productos: carrito.productos,
+        total: totalPrecio.value
+      }
+    });
+
+    alert("¡Gracias por tu compra! Pedido nº: " + respuesta.pedidoId);
+
+    
+    navigateTo("/");
+    vaciarCarrito();
+
+  } catch (error) {
+    alert("Hubo un error al procesar el pedido");
+  }
+};
+</script>
