6f5614b9da65e50e4200920d88d64983c9aed286
Ramongl <ramongarcia1988@gmail.com>
2026-05-02 18:19:41 +0200

    Update Cabecera.vue
    Adaptación de la plantilla cabecera para que incluya la cantidad en el carrito, se elimina barra de busqueda y se sustituye por logo uax para volver al inicio. Se homogeneiza los estilos y se añade la cookie para saber si el usuario ya esta logado.

--- app/components/Cabecera.vue
+++ app/components/Cabecera.vue
@@ -1,34 +1,47 @@
-
 <template>
   <header
     class="w-full border-b border-gray-200 shadow-2xs bg-white sticky top-0 z-1"
   >
-    
-    
     <div class="max-w-3x2 mx-auto px-10">
       <div class="flex items-center justify-between h-20 font-bold">
-        
         <div class="flex items-center gap-8">
-          <button class="md:hidden" @click="menuMovil = !menuMovil">
-            ☰
-          </button>
+          <button class="md:hidden" @click="menuMovil = !menuMovil">☰</button>
         </div>
-        <div class="flex-1 max-w-2xl mx-10">
-          <input
-            type="text"
-            placeholder="Buscar..."
-            class="w-full rounded-full border border-gray-300 px-6 py-2 focus:outline-none focus:ring-2 focus:ring-pink-300"
-          />
+        <div class="flex justify-center">
+          <NuxtLink to="/"><img src="/assets/imagenes/logo.png" /></NuxtLink>
         </div>
-       
-        <div class="flex items-center gap-4 md md:gap-7 ">
-          <NuxtLink to="/cuenta" class="relative hover:text-rose-500">👤</NuxtLink>
+
+        <div class="flex items-center gap-4 md md:gap-7">
+          <div>
+            <NuxtLink
+              v-if="estado == 0"
+              to="/cuenta"
+              class="relative hover:text-rose-500"
+              >👤</NuxtLink
+            >
+            <span v-if="estado == 1"> {{ authCookie.nombre }}</span>
+            <NuxtLink
+              v-if="estado == 1"
+              to="/"
+              @click="borrarCookie"
+              class="relative hover:text-rose-500"
+            >
+              (Salir)</NuxtLink
+            >
+            <NuxtLink
+              v-if="admonCheck"
+              to="/administracion"
+              class="relative hover:text-rose-500"
+            >
+              (Panel Admin.)</NuxtLink
+            >
+          </div>
           <NuxtLink to="/carrito" class="relative hover:text-rose-500">
             🛒
             <span
               class="absolute -bottom-2 -left-2 bg-pink-500 text-white text-xs rounded-full px-1"
             >
-              0
+              {{ totalCantidades }}
             </span>
           </NuxtLink>
         </div>
@@ -39,41 +52,61 @@
       v-if="menuMovil"
       class="md:hidden bg-white border-t px-4 py-4 space-y-3 z-10"
     >
-      <NuxtLink to="/cosmetica" class="block font-bold text-black-400 hover:text-rose-500 uppercase"
+      <NuxtLink
+        to="/cosmetica"
+        class="block font-bold text-black-400 hover:text-rose-500 uppercase"
         >Cosmética</NuxtLink
       >
-      <NuxtLink to="/maquillaje" class="block font-bold text-black-400 hover:text-rose-500 uppercase"
+      <NuxtLink
+        to="/maquillaje"
+        class="block font-bold text-black-400 hover:text-rose-500 uppercase"
         >Maquillaje</NuxtLink
       >
-      <NuxtLink to="/perfumes" class="block font-bold text-black-400 hover:text-rose-500 uppercase"
+      <NuxtLink
+        to="/perfumes"
+        class="block font-bold text-black-400 hover:text-rose-500 uppercase"
         >Perfumes</NuxtLink
       >
-      <NuxtLink to="/cabello" class="block font-bold text-black-400 hover:text-rose-500 uppercase"
+      <NuxtLink
+        to="/cabello"
+        class="block font-bold text-black-400 hover:text-rose-500 uppercase"
         >Cabello</NuxtLink
       >
-      <NuxtLink to="/higiene" class="block font-bold text-black-400 hover:text-rose-500 uppercase"
+      <NuxtLink
+        to="/higiene"
+        class="block font-bold text-black-400 hover:text-rose-500 uppercase"
         >Higiene</NuxtLink
       >
     </div>
   </header>
-  <div class="w-full border-b border-gray-200 shadow-2xs bg-white sticky top-20 z-1">
-    <nav
-      class="hidden md:flex border-b border-gray-200 shadow-2xs py-5 "
-    >
+  <div
+    class="w-full border-b border-gray-200 shadow-2xs bg-white sticky top-20 z-1"
+  >
+    <nav class="hidden md:flex border-b border-gray-200 shadow-2xs py-5">
       <div class="text-xl md:flex font-bold gap-20 mx-auto justify-center">
-        <NuxtLink to="/cosmetica" class="text-black-400 hover:text-rose-500 uppercase"
+        <NuxtLink
+          to="/cosmetica"
+          class="text-black-400 hover:text-rose-500 uppercase"
           >Cosmética</NuxtLink
         >
-        <NuxtLink to="/maquillaje" class="text-black-400 hover:text-rose-500 uppercase"
+        <NuxtLink
+          to="/maquillaje"
+          class="text-black-400 hover:text-rose-500 uppercase"
           >Maquillaje</NuxtLink
         >
-        <NuxtLink to="/perfumes" class="text-black-400 hover:text-rose-500 uppercase"
+        <NuxtLink
+          to="/perfumes"
+          class="text-black-400 hover:text-rose-500 uppercase"
           >Perfumes</NuxtLink
         >
-        <NuxtLink to="/cabello" class="text-black-400 hover:text-rose-500 uppercase"
+        <NuxtLink
+          to="/cabello"
+          class="text-black-400 hover:text-rose-500 uppercase"
           >Cabello</NuxtLink
         >
-        <NuxtLink to="/higiene" class="text-black-400 hover:text-rose-500 uppercase"
+        <NuxtLink
+          to="/higiene"
+          class="text-black-400 hover:text-rose-500 uppercase"
           >Higiene</NuxtLink
         >
       </div>
@@ -83,5 +116,32 @@
 
 <script setup>
 import { ref } from "vue";
+import { totalCantidades } from '../utils/carrito'
+
 const menuMovil = ref(false);
+const authCookie = useCookie("auth_user");
+const admonCheck = ref(false);
+var estado = 0;
+
+const mirarCookie = () => {
+  if (authCookie.value) {
+    estado = 1;
+    if (authCookie.value.rol == "admon") {
+      admonCheck.value= true;
+    } else {
+      admonCheck.value = false;
+    }
+  } else {
+    estado = 0;
+  }
+};
+
+const borrarCookie = () => {
+  authCookie.value = null;
+  estado = 0;
+  admonCheck.value = false;
+  mirarCookie();
+};
+
+mirarCookie();
 </script>
