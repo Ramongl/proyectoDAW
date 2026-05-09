0c55a9a3f98f5bd6ebed6a66c403295d1c91e3ad
Ramongl <ramongarcia1988@gmail.com>
2026-05-09 20:07:28 +0200

    Actualización admon ped
    Se edita la estructura de admon ped para darle usabilidad

--- app/components/AdmonPed.vue
+++ app/components/AdmonPed.vue
@@ -10,63 +10,50 @@
 
       <div class="space-y-4">
         <div
-          class="flex justify-items-start container items-center pb-5 gap-10"
-        >
-          <div class="grid justify-items-center w-full">
-            <div class="flex-1 max-w-2xl mx-10">
-              <input
-                type="text"
-                placeholder="Buscar..."
-                class="md:w-[500px] sm:flex-grow sm:max-w-2xl border border-gray-300 p-3 rounded-full text-sm focus:outline-none focus:border-rose-400"
-              />
-            </div>
-          </div>
-          <button
-            class="bg-rose-400 text-white px-6 py-2.5 text-[10px] font-bold uppercase hover:bg-black"
-          >
-            Buscar
-          </button>
-        </div>
-        <div
-          v-for="i in 50"
-          :key="i"
+          v-for="ped in pedidos"
+          :key="ped.id"
           class="group bg-white border border-black-100 p-6 flex flex-col lg:flex-row lg:items-center justify-between gap-6"
         >
           <div class="flex gap-6 items-center">
             <div>
               <div class="flex items-center gap-2 mb-1">
                 <span class="text-[10px] font-bold text-rose-400"
-                  >#PEDIDO-2026-00{{ i }}</span
+                  >#PEDIDO {{ ped.id }}</span
                 >
                 <span
+                  v-if="ped.estado"
+                  :class="{
+                    'bg-green-100 text-green-800 border-green-200':
+                      ped.estado == 'Procesado',
+                    'bg-red-100 text-red-800 border-red-200':
+                      ped.estado == 'Cancelado',
+                  }"
                   class="px-2 py-0.5 bg-amber-50 text-amber-600 text-[9px] font-bold uppercase"
-                  >Pendiente</span
+                  >{{ ped.estado }}</span
                 >
               </div>
             </div>
           </div>
 
           <div class="flex flex-col lg:items-end gap-1">
-            <p class="text-lg font-medium text-black-900">Importe,00 €</p>
-            <p class="text-[10px] text-black-400 uppercase">Fecha y hora</p>
+            <p class="text-lg font-medium text-black-900">{{ ped.total }} €</p>
+            <p class="text-[10px] text-black-400 uppercase">{{ ped.fecha }}</p>
           </div>
 
           <div class="flex gap-2">
             <button
+              v-if="ped.estado === 'Pendiente'"
+              @click="actualizarEstadoPedido(ped.id, 'Procesado')"
               class="flex-1 lg:flex-none bg-green-500 text-white px-6 py-2.5 text-[10px] font-bold uppercase hover:bg-black"
             >
               Aceptar
             </button>
             <button
+              v-if="ped.estado === 'Pendiente'"
+              @click="actualizarEstadoPedido(ped.id, 'Cancelado')"
               class="flex-1 lg:flex-none bg-red-500 text-white px-6 py-2.5 text-[10px] font-bold uppercase hover:bg-black"
             >
               Rechazar
-            </button>
-            <div class="border"></div>
-            <button
-              class="flex-1 lg:flex-none bg-amber-500 text-white px-6 py-2.5 text-[10px] font-bold uppercase hover:bg-black"
-            >
-              Ver detalle
             </button>
           </div>
         </div>
@@ -74,3 +61,41 @@
     </div>
   </section>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const pedidos = ref([]);
+
+const obtenerPedidos = async () => {
+  try {
+    const data = await $fetch("/api/buscar-todos-pedidos", {
+      method: "GET",
+    });
+
+    pedidos.value = data;
+  } catch (error) {
+    console.error("Error al obtener pedidos:", error);
+    alert("No se pudieron cargar los pedidos");
+  } finally {
+  }
+};
+obtenerPedidos();
+
+const actualizarEstadoPedido = async (id, nuevoEstado) => {
+  try {
+    await $fetch("/api/actualizar-estado", {
+      method: "PATCH",
+      body: {
+        pedido_id: id,
+        nuevo_estado: nuevoEstado,
+      },
+    });
+
+    obtenerPedidos();
+  } catch (error) {
+    console.error("Error al cambiar estado:", error);
+    alert("No se pudo cambiar el estado del pedido");
+  }
+};
+</script>
